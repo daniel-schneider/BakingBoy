1dd3351295e15d52ffd2e1f6feeaad69f483854d
Daniel Schnieder <daniel.schneider516@gmail.com>
2018-08-05 15:11:59 -0500

    created a couple POJOs

---
+++ app/src/main/java/boy/bake/bakingboy/recipe/Ingredient.java
@@ -0,0 +1,45 @@
+package boy.bake.bakingboy.recipe;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ingredient {
+
+    private Integer quantity;
+    private String measure;
+    private String ingredient;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getMeasure() {
+        return measure;
+    }
+
+    public void setMeasure(String measure) {
+        this.measure = measure;
+    }
+
+    public String getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(String ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
